6c09e444d6fadc23b3ffd778a8ce085bc45df0f9
Kukuh Frehadtomo <kufhed@gmail.com>
2021-03-31 15:56:43 +0700

    update

--- installChecker.sh
+++ installChecker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Installing..."
 url = "https://raw.githubusercontent.com/kufhed/xderm-checker/main"
-wget -O /www/xderm/xdermChecker.py $url/xdermChecker.py -q
+wget -O /root/xdermChecker.py $url/xdermChecker.py -q
 #write out current crontab
 crontab -l > mycron
 #echo new cron into cron file
